221ddc3ae6e44fbd6c833deb149ef875ad915e31
Igor Sasic <154253059+SasicIgor@users.noreply.github.com>
2025-03-13 17:23:59 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SearchForm from "./components/form/SearchForm";
 import UserProfile from "./components/user/UserProfile";
 
 const App = () => {
-  const [userName, setUserName] = useState("quincylarson");
+  const [userName, setUserName] = useState("ivansasic");
   return (
     <main className="mx-auto max-w-6xl px-8 py-20">
       <SearchForm userName={userName} setUserName={setUserName} />
